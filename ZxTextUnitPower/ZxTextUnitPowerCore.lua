004bde11bdd79be83e1bd7daeb20915fea45b036
toddnguyen11 <todd.nguyen47@gmail.com>
2017-12-18 23:51:35 -0800

    Added Target Health Bar

--- ZxTextUnitPower/ZxTextUnitPowerCore.lua
+++ ZxTextUnitPower/ZxTextUnitPowerCore.lua
@@ -1,3 +1,19 @@
+local function enableFrameMovement(frameInput)
+	frameInput:SetMovable(true);
+	frameInput:EnableMouse(true);
+	frameInput:RegisterForDrag("LeftButton");
+	frameInput:SetScript("OnDragStart", frameInput.StartMoving);
+	frameInput:SetScript("OnDragStop", frameInput.StopMovingOrSizing);
+end
+
+local function createBlackBg(frame, alphaLevel)
+	frame.texture1 = frame:CreateTexture(nil, "BACKGROUND");
+	frame.texture1:SetTexture(0, 0, 0, alphaLevel);
+	frame.texture1:SetAllPoints();
+	
+	return frame.texture1;
+end
+
 local function shortenNumber(number)
 	local strNumber = nil
 
@@ -30,7 +46,7 @@ local function getRgb()
 		end
 	end
 
-	return r, g, b
+	return r, g, b, powerToken
 end
 
 -- Wrapper function to register for multipke power types
@@ -40,8 +56,13 @@ end
 
 -- Wrapper function to color texture
 local function drawTexture()
-	r, g, b = getRgb();
-	ZxMasterFrame.MainFrame.texture1:SetTexture(r, g, b, 0.6);
+	r, g, b, powerToken = getRgb();
+	if (powerToken == "MANA") then
+		ZxMasterFrame.MainFrame.texture1:SetTexture(r, g, b, 0.5);
+	else
+		ZxMasterFrame.MainFrame.texture1:SetTexture(r, g, b, 0.7);
+	end 
+
 	ZxMasterFrame.MainFrame.texture1:SetAllPoints();
 end
 
@@ -51,6 +72,15 @@ powerRegistered = {
 	"UNIT_ENERGY",
 	"UNIT_RUNIC_POWER"
 }
+
+local function writePowerValue(value)
+	if (value == "UNIT_MANA" or value == "MANA") then
+		percentage = string.format("%.1f %%", UnitPower("Player")/UnitPowerMax("Player") * 100);
+		ZxMasterFrame.MainFrame.UnitPowerDisplay:SetText(percentage);
+	else
+		ZxMasterFrame.MainFrame.UnitPowerDisplay:SetText(shortenNumber(UnitPower("Player")));
+	end
+end
 
 local function hasValue(value)
 	for _, power in pairs(powerRegistered) do
@@ -67,9 +97,10 @@ local function createUnitDisplay()
 	drawTexture()
 	
 	ZxMasterFrame.MainFrame.UnitPowerDisplay = ZxMasterFrame.MainFrame:CreateFontString(nil, "OVERLAY");
-	ZxMasterFrame.MainFrame.UnitPowerDisplay:SetFont("Interface\\AddOns\\ActionBarFont\\ptsans-bold.ttf", 16, "OUTLINE");
+	ZxMasterFrame.MainFrame.UnitPowerDisplay:SetFont("Interface\\AddOns\\ZxTextUnitPower\\PTSansBold.ttf", 16, "OUTLINE");
 	ZxMasterFrame.MainFrame.UnitPowerDisplay:SetAllPoints();
-	ZxMasterFrame.MainFrame.UnitPowerDisplay:SetText(shortenNumber(UnitPower("Player")));
+	_, unitPType = UnitPowerType("Player")
+	writePowerValue(unitPType)
 
 	for _, power in pairs(powerRegistered) do 
 		registerForEvents(power)
@@ -79,27 +110,76 @@ local function createUnitDisplay()
 	
 	ZxMasterFrame.MainFrame:SetScript("OnEvent", function(self, event, unit)
 		if (hasValue(event)) then
-			ZxMasterFrame.MainFrame.UnitPowerDisplay:SetText(shortenNumber(UnitPower("Player")));
+			writePowerValue(event);
 		else
 			drawTexture()
 		end
 	end)
 end
 
+local function createTargetHp()
+	local frame1 = CreateFrame("Frame", "ZxTargetHealth", ZxMasterFrame);
+	enableFrameMovement(frame1);
+	
+	frame1.texture1 = createBlackBg(frame1, 0.8);
+	frame1:SetWidth(75);
+	frame1:SetHeight(20);
+	frame1:SetPoint("LEFT", PlayerFrame, "RIGHT", 40, 0);
+	
+	frame1.targetHp = CreateFrame("StatusBar", nil, ZxTargetHealth);
+	frame1.targetHp:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar");
+	frame1.targetHp:GetStatusBarTexture():SetHorizTile(false);
+	frame1.targetHp:SetAllPoints();
+	frame1.targetHp:SetStatusBarColor(0, 1, 0, 0.5);
+	
+	frame1.hpText = frame1:CreateFontString(nil, "OVERLAY");
+	frame1.hpText:SetFont("Interface\\AddOns\\ZxTextUnitPower\\PTSansBold.ttf", 16, "OUTLINE");
+	frame1.hpText:SetText(1.0, 1.0, 1.0, 1.0);
+	frame1.hpText:SetAllPoints();
+	
+	frame1:Hide(); -- No target selected initially
+	
+	frame1.targetHp:RegisterEvent("PLAYER_TARGET_CHANGED");
+	frame1.targetHp:RegisterEvent("UNIT_HEALTH");
+	frame1.targetHp:SetScript("OnEvent", function(self, event, unit)
+		if (event == "PLAYER_TARGET_CHANGED") then
+			local curHp = UnitHealth("Target");
+			local maxHp = UnitHealthMax("Target");
+			local percentHp = curHp / maxHp * 100;
+			-- If no target selected, then hide the frames
+			if (maxHp == 0) then
+				frame1:Hide();
+				return;
+			-- If target is selected, show frames
+			else
+				frame1.targetHp:SetMinMaxValues(0, 100);
+				frame1.targetHp:SetValue(percentHp);
+				frame1.hpText:SetText(string.format("%.1f%%", percentHp));
+				frame1:Show();
+			end
+			
+		elseif (event == "UNIT_HEALTH") then
+			local curHp = UnitHealth("Target");
+			local maxHp = UnitHealthMax("Target");
+			local percentHp = curHp / maxHp * 100;
+			frame1.targetHp:SetValue(percentHp);
+			frame1.hpText:SetText(string.format("%.1f%%", percentHp));
+		end
+	end)
+end
 
 local function init()
 	ZxMasterFrame = CreateFrame("Frame", "ZxMasterFrame", UIParent);
+	ZxMasterFrame:SetPoint("CENTER", 0, 0);
 	
 	ZxMasterFrame.MainFrame = CreateFrame("Frame", "MainFrame", ZxMasterFrame)
-	ZxMasterFrame.MainFrame:SetSize(70,25)
+	ZxMasterFrame.MainFrame:SetSize(75,20)
 	ZxMasterFrame.MainFrame:SetPoint("BOTTOMLEFT", PlayerFrame, "TOP", -20, -15)
-	ZxMasterFrame.MainFrame:SetMovable(true)
-	ZxMasterFrame.MainFrame:EnableMouse(true)
-	ZxMasterFrame.MainFrame:RegisterForDrag("LeftButton")
-	ZxMasterFrame.MainFrame:SetScript("OnDragStart", ZxMasterFrame.MainFrame.StartMoving)
-	ZxMasterFrame.MainFrame:SetScript("OnDragStop", ZxMasterFrame.MainFrame.StopMovingOrSizing)
-
+	enableFrameMovement(ZxMasterFrame.MainFrame);
+	
 	createUnitDisplay()
+	
+	createTargetHp();
 end
 
 
